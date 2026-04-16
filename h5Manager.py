b7073f2aac9d744af626b8ccc4fea50007ee7e6f
ejporterSU <ejporter@stanford.edu>
2026-04-16 10:41:26 -0700

    made h5Manager more general

--- h5Manager.py
+++ h5Manager.py
@@ -12,7 +12,8 @@ class ExpViewer:
         self.dir = dir if dir is not None else os.getcwd() # sets internal directory
         self.path = self.find_RID(RID) # attempts to find path
         print(f"found RID at: {self.path}")
-        self.df = self.get_h5(RID) # h5 file as a dataframe
+        self.df = self.get_h5() # h5 file as a dataframe
+
 
         # extract surface info
         self.rid = self.df['rid'][()]
@@ -30,29 +31,21 @@ class ExpViewer:
         archive_vars = list(self.df['archive'].keys())
         archive_vals = [self.df[f'archive/{var}'][()] for var in archive_vars]
         self.archive = dict(zip(archive_vars, archive_vals))
-        
-        # grabs a list of all scanned parameters
-        self.scans = []
-        for key, val in self.parameters.items():
-            if isinstance(val, dict):
-                self.scans.append(self.dict_to_array(val))
-
-        datasets = list(self.df['datas  ets'].keys())
-
-        if len(self.scans) > 1:
-            self.x_vals = np.zeros((len(self.scans[0]), len(self.scans[1]), 2))
-            self.y_vals = np.zeros((len(self.scans[0]), len(self.scans[1])))
-            for i, val1 in enumerate(self.scans[0]):
-                for j, val2 in enumerate(self.scans[1]):
-                    self.x_vals[i, j, :] = [val1, val2]
-                    self.y_vals[i][j] = self.df[f'datasets/PhaseMeasurement_{i}_{j}'][()]
-
-    
-
+ 
+   
+        datasets = list(self.df['datasets'].keys())
 
         self.camera_used = (np.char.find(datasets, ".images.") > -1).any()
         self.scan_used = (np.char.find(datasets, "current_scan.") > -1).any()
         self.fit_used = (np.char.find(datasets, ".fits.") > -1).any()
+
+        if self.scan_used:
+            is_2d = 'current_scan.plots.dim1.x' in datasets
+            if is_2d:
+                self.x1 = self.df['datasets/current_scan.plots.dim1.x'][()]
+                self.x0 = self.df['datasets/current_scan.plots.x'][()]
+            else:
+                self.x = self.df['datasets/current_scan.plots.x'][()]
 
         # extracts all images
         if self.camera_used:
@@ -71,39 +64,13 @@ class ExpViewer:
                     pass
                 ind += 1
 
-        if self.scan_used:
-            self.name_space = None
-            self.plot_title = None
-            self.x = None
-            self.x_units = None
-            self.x_label = None
-            self.x_scale = None
-            self.y = None
-            self.y_units = None
-            self.y_label = None
-            self.y_scale = None
-            self.extract_plot()
 
-        if self.fit_used:
-            self.fit_info = {}
-            for ds in datasets:
-                if "current_scan.fits." in ds and "guess" not in ds:
-                    self.fit_info[ds.split('.')[-1]] = self.df[f'datasets/{ds}'][()]
-
-        self.saved_data = {}
+        skip_words = ['image']
+        self.data = {}
         for ds in datasets:
-            if "current_scan" not in ds and "image" not in ds and "plots" not in ds and "fits" not in ds:
-                self.saved_data[ds.split('.')[-1]] = self.df[f'datasets/{ds}'][()]
+            if not any(word in ds for word in skip_words):
+                self.data[ds] = self.df[f'datasets/{ds}'][()]
 
-    def help(self):
-        print("""Available variables:
-                \tdir\n\tpath\n\tdf\n\trid\n\tstart_time\n\trun_time\n\tfile\n\tclass_name
-                \tarchive\n\tparameters\n\tscans\n\tbackground_image\n\timages\n\traw_images
-                \tfit_info\n\tsaved_data
-
-                Available Methods:
-                \tfind_RID\n\tget_h5\n\tdict_to_array\n\t__str__\n\textract_plot\n\tvals_from_images
-                """)
 
     def find_RID(self, RID):
         RID = str(RID)
@@ -111,10 +78,13 @@ class ExpViewer:
             for f in files:
                 if RID in f and '.csv' not in f:
                     return root + '/' + f
+        raise Exception(f"No RID ({RID}) found...")
 
-    def get_h5(self, RID):
-        path = self.find_RID(RID)
-        return h5py.File(path, 'r')
+    def get_h5(self):
+        return h5py.File(self.path, 'r')
+
+    def close(self):
+        self.df.close()
 
     def dict_to_array(self, myDict):
         """
@@ -142,24 +112,11 @@ class ExpViewer:
 
         return res
 
-    def extract_plot(self):
-        try:
-            self.name_space = ''
-            self.plot_title = self.df['datasets/current_scan.plots.plot_title'][()]
-            self.x = self.df['datasets/current_scan.plots.x'][()]
-            self.x_units = self.df['datasets/current_scan.plots.x_units'][()]
-            self.x_label = self.df['datasets/current_scan.plots.x_label'][()]
-            self.x_scale = self.df['datasets/current_scan.plots.x_scale'][()]
-            self.y = self.df['datasets/current_scan.plots.y'][()]
-            self.y_units = self.df['datasets/current_scan.plots.y_units'][()]
-            self.y_label = self.df['datasets/current_scan.plots.y_label'][()]
-            self.y_scale = self.df['datasets/current_scan.plots.y_scale'][()]
-        except Exception:
-            print('No Scan to Generate Plot With')
-
-
 if __name__ == "__main__":
-    RID = 75270
+    RID = 75270 # 2D
+    # RID = 75205 # 1D
     _DATA_DIREC = "C:/Users/Erik/Desktop/Kasevich Lab/ThreePhotonSimulations\Data"
     exp = ExpViewer(RID, dir=_DATA_DIREC)
+
+
 
